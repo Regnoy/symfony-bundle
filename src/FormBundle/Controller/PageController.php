3e0b22178e4efda2118145281b4a4bb998add9dc
Regnoy <pavel.culacov@gmail.com>
2017-12-01 11:24:18 +0200

    Lessong Form #6

--- src/FormBundle/Controller/PageController.php
+++ src/FormBundle/Controller/PageController.php
@@ -9,25 +9,39 @@
 namespace FormBundle\Controller;
 
 
+use FormBundle\Entity\FormProduct;
+use FormBundle\Forms\FormProductForm;
 use FormBundle\Forms\Models\TaskModel;
 use FormBundle\Forms\TaskForm;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class PageController extends Controller {
-
   public function exampleAction($example, Request $request){
-    $taskModel = new TaskModel();
-    $taskForm = $this->createForm( TaskForm::class, $taskModel );
-    $taskForm->handleRequest($request);
-    if($taskForm->isSubmitted()){
-
-      var_dump($taskModel->getTask());
-      //save
+    $formProduct = new FormProduct();
+    $form = $this->createForm( FormProductForm::class, $formProduct );
+    $form->handleRequest($request);
+    if($form->isSubmitted()){
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($formProduct);
+      $em->flush();
       return $this->redirectToRoute('form_example');
     }
     return $this->render('FormBundle::form.html.twig', [
-      'form' => $taskForm->createView()
+      'form' => $form->createView()
     ]);
   }
+//  public function exampleAction($example, Request $request){
+//    $taskModel = new TaskModel();
+//    $taskForm = $this->createForm( TaskForm::class, $taskModel );
+//    $taskForm->handleRequest($request);
+//    if($taskForm->isSubmitted()){
+//      var_dump($taskModel->getTask());
+//      //save
+//      return $this->redirectToRoute('form_example');
+//    }
+//    return $this->render('FormBundle::form.html.twig', [
+//      'form' => $taskForm->createView()
+//    ]);
+//  }
 }
---
+++ src/FormBundle/Entity/FormProduct.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace FormBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Page
+ * @ORM\Entity
+ * @ORM\Table(name="form_product")
+ */
+class FormProduct {
+
+  /**
+   * @ORM\Column(type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
+
+  /**
+   * @ORM\Column(type="string", length=255)
+   */
+
+  private $title;
+
+  /**
+   * @ORM\Column(type="text")
+   */
+  private $description;
+
+  /**
+   * @ORM\Column(type="float")
+   */
+  private $price;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return FormProduct
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return FormProduct
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     *
+     * @return FormProduct
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
